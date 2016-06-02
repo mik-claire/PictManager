2d677945d7eb901c17dcbf0dfba2c31e8cb4f326
N-Nishimura <namihiro.nishimura@smart-foa.com>
2016-06-02 18:30:52 +0900

    Implemented to select, insert and update DB.

--- PictManager/View/MainWindow.xaml.cs
+++ PictManager/View/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace PictManager
 
         private void load()
         {
-            var dbData = loadDB(PmUtil.DB_PATH);
+            var dbData = loadDB(PmUtil.DB_PATH, string.Empty);
 
             if (!Directory.Exists(PmConf.Config.DirectoryPath))
             {
@@ -83,11 +83,137 @@ namespace PictManager
                 infoList.Add(pi);
             }
 
+            insert(infoList);
             this.DataContext = infoList;
         }
 
-        private List<PictureInfo> loadDB(string filePath)
+        private void search()
         {
+            if (this.textBox_Search.Text.Trim() == string.Empty)
+            {
+                // Reload
+                load();
+                return;
+            }
+
+            string cond = string.Empty;
+            string column = string.Empty;
+            string[] words = this.textBox_Search.Text.Trim().Split(' ');
+            if ((bool)this.radioButton_FileName.IsChecked)
+            {
+                column = "fileName";
+            }
+            else if ((bool)this.radioButton_DisplayName.IsChecked)
+            {
+                column = "displayName";
+            }
+            else // if ((bool)this.radioButton_Tags.IsCheched)
+            {
+                column = "tags";
+            }
+
+            foreach (string word in words)
+            {
+                if (cond != string.Empty)
+                {
+                    cond += " or ";
+                }
+                cond = string.Format("{0} like '%{1}%'", column, word);
+            }
+
+            var dbData = loadDB(PmUtil.DB_PATH, cond);
+
+            if (!Directory.Exists(PmConf.Config.DirectoryPath))
+            {
+                Directory.CreateDirectory(PmConf.Config.DirectoryPath);
+            }
+
+            this.DataContext = dbData;
+        }
+
+        #region Event Handler
+
+        /// <summary>
+        /// Load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PmConf.LoadConfig();
+
+                load();
+            }
+            catch (Exception ex)
+            {
+                PmUtil.ShowError(ex);
+            }
+        }
+
+        /// <summary>
+        /// [ TagSet ]
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_TagSet_Click(object sender, RoutedEventArgs e)
+        {
+            var piObj = this.listView_Picts.SelectedItem;
+            PictureInfo pi = piObj as PictureInfo;
+            if (pi == null)
+            {
+                return;
+            }
+
+            List<string> tags = this.textBox_Tags.Text.Trim().Split(' ').ToList();
+            pi.Tags = tags;
+
+            updateTags(pi.Id, string.Join(" ", tags));
+
+            search();
+        }
+
+        /// <summary>
+        /// [ Search ]
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Search_Click(object sender, RoutedEventArgs e)
+        {
+            search();
+        }
+
+        /// <summary>
+        /// ListView selection changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listView_Picts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var piObj = this.listView_Picts.SelectedItem;
+            PictureInfo pi = piObj as PictureInfo;
+            if (pi == null)
+            {
+                return;
+            }
+
+            string tags = string.Join(" ", pi.Tags);
+            this.textBox_Tags.Text = tags;
+        }
+
+        #endregion
+
+        #region DB Control
+
+        private List<PictureInfo> loadDB(string filePath, string cond)
+        {
+            string sql = @"
+select
+  *
+from
+  data";
+
             List<PictureInfo> infoList = new List<PictureInfo>();
 
             SQLiteConnection cn = null;
@@ -99,9 +225,17 @@ namespace PictManager
                 cn = new SQLiteConnection(PmUtil.CONNECTION_STRING);
                 cn.Open();
 
-                cmd = new SQLiteCommand(string.Format(
-@"select * from data", ""),
-                    cn);
+                if (cond != string.Empty)
+                {
+                    sql += @"
+where
+  {0}
+";
+                    sql = string.Format(sql,
+                        cond);
+                }
+
+                cmd = new SQLiteCommand(sql + ";", cn);
 
                 reader = cmd.ExecuteReader();
 
@@ -111,7 +245,7 @@ namespace PictManager
                     pi.Id = int.Parse(reader["id"].ToString());
                     pi.FileName = reader["fileName"].ToString();
                     pi.DisplayName = reader["displayName"].ToString();
-                    string[] tags = reader["tags"].ToString().Split(',');
+                    string[] tags = reader["tags"].ToString().Split(' ');
                     pi.Tags = tags.ToList();
                     pi.IsNewPicture = false;
 
@@ -139,21 +273,94 @@ namespace PictManager
             return infoList;
         }
 
-        #region Event Handler
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void insert(List<PictureInfo> piList)
         {
+            SQLiteConnection cn = null;
+            SQLiteCommand cmd = null;
+
             try
             {
-                PmConf.LoadConfig();
+                cn = new SQLiteConnection(PmUtil.CONNECTION_STRING);
+                cn.Open();
 
-                load();
+                foreach (PictureInfo pi in piList)
+                {
+                    if (!pi.IsNewPicture)
+                    {
+                        continue;
+                    }
+
+                    string sql = @"
+insert into data (
+  fileName,
+  displayName,
+  tags
+)
+values (
+  '{0}',
+  '{1}',
+  '{2}'
+);";
+
+                    string tags = string.Join(" ", pi.Tags);
+                    sql = string.Format(sql,
+                        pi.FileName,
+                        pi.DisplayName,
+                        tags);
+
+                    cmd = new SQLiteCommand(sql, cn);
+
+                    cmd.ExecuteNonQuery();
+                }
             }
-            catch (Exception ex)
+            finally
             {
-                PmUtil.ShowError(ex);
-            }
+                if (cmd != null)
+                {
+                    cmd.Dispose();
+                }
 
+                if (cn != null)
+                {
+                    cn.Close();
+                }
+            }
+        }
+
+        private void updateTags(int index, string tags)
+        {
+            string sql =@"
+update data
+  set tags = '{0}'
+where id = {1};";
+
+            SQLiteConnection cn = null;
+            SQLiteCommand cmd = null;
+
+            try
+            {
+                cn = new SQLiteConnection(PmUtil.CONNECTION_STRING);
+                cn.Open();
+
+                sql = string.Format(sql,
+                    tags,
+                    index);
+                cmd = new SQLiteCommand(sql, cn);
+
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                if (cmd != null)
+                {
+                    cmd.Dispose();
+                }
+
+                if (cn != null)
+                {
+                    cn.Close();
+                }
+            }
         }
 
         #endregion
